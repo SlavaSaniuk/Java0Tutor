87e6b3c407843f5373b1ec2656fe72d919e1c55f
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2022-09-12 11:10:27 +0300

    Worked and tested branch task 4, 5

---
+++ src/by/saniuk/conditional/BranchTaskFour.java
@@ -0,0 +1,28 @@
+package by.saniuk.conditional;
+
+public class BranchTaskFour {
+
+    private final double a,b;
+    private double x,y,z;
+
+    public BranchTaskFour(double anA, double aB) {
+        this.a = anA;
+        this.b = aB;
+    }
+
+    public void setBrickSize(double aX, double aY, double aZ) {
+        this.x = aX;
+        this.y = aY;
+        this.z = aZ;
+    }
+
+    public boolean isBrickGoThrowHole() {
+        // Check X-Y side:
+        if (((a>=x) && (b>=y)) || ((a>=y) && (b>=x))) return true;
+        // Check X-Z side:
+        if (((a>=x) && (b>=z)) || ((a>=z) && (b>=x))) return true;
+        // Check Y-Z side:
+        return ((a >= y) && (b >= z)) || ((a >= z) && (b >= y));
+    }
+
+}
